a9d5f56800dc3a5ed02ef076c018616f088c9143
Anup <arpandey@gmail.com>
2022-06-21 06:23:04 -0500

    [062212022] Updated the printeventhread and semaphore class

---
+++ java/Z/com/anup/companiesSamples/SemaphoreSharedResource.java
@@ -0,0 +1,68 @@
+package Z.com.anup.Concurrency;
+
+//https://www.netjstech.com/2016/02/semaphore-in-java-concurrency.html
+
+import java.util.concurrent.Semaphore;
+
+import java.util.concurrent.Semaphore;
+
+public class SemaphoreSharedResource {
+  public static void main(String[] args) {
+    Semaphore sem = new Semaphore(1);/////////////////********number of permits
+    SemThread st = new SemThread(sem);//****************
+    // Creating three threads
+    Thread t1 = new Thread(st, "Thread-A");// Thread instances
+    Thread t2 = new Thread(st, "Thread-B");
+    Thread t3 = new Thread(st, "Thread-C");
+    t1.start();
+    t2.start();
+    t3.start();
+  }
+}
+
+
+//Thread class with semaphore and counter variables and incrment/decrement methods inside run 
+class SemThread implements Runnable{
+  private int count = 0;
+  private Semaphore sem;
+  SemThread(Semaphore s){
+    this.sem = s;
+  }
+  // incrementing the value
+  public void increment() {
+    try {
+      // used sleep for context switching
+      Thread.sleep(10);
+    } catch (InterruptedException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    count++;
+  }
+  // decrementing the value
+  public void decrement() {    
+    count--;
+  }
+
+  public int getValue() {
+    return count;
+  }
+    
+  @Override
+  public void run() {
+    try {
+      // acquire method to get one permit
+      sem.acquire();
+      this.increment();
+      System.out.println("Value for Thread After increment - " + Thread.currentThread().getName() + ":: " + this.getValue());
+      this.decrement();
+      System.out.println("Value for Thread at last " + Thread.currentThread().getName() + ":: " + this.getValue());
+      // releasing permit
+      sem.release();
+    }
+    catch (InterruptedException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
+}
